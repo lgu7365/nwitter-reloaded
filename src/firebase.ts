8faa4d763d9749735922d8146afee2907e56222e
unknown <lgu_lgu@naver.com>
2023-10-30 21:33:41 +0900

    router

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDG0yXsaSKPcOAy7chmFBS-Y_7V5QunZKw",
+  authDomain: "nwitter-reloaded-6ec0b.firebaseapp.com",
+  projectId: "nwitter-reloaded-6ec0b",
+  storageBucket: "nwitter-reloaded-6ec0b.appspot.com",
+  messagingSenderId: "438451952680",
+  appId: "1:438451952680:web:8610f8fcb3b52d6affa91d"
+};
+
+const app = initializeApp(firebaseConfig);
