a8c80b1edd49b7e9507aca1043e737e403424fcf
unknown <lgu_lgu@naver.com>
2023-11-06 21:03:22 +0900

    create-account

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,11 @@ import Home from "./routes/home"
 import Profile from "./routes/profile"
 import Login from "./routes/login"
 import CreateAccount from "./routes/create-account"
-import { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 import { useEffect, useState } from "react"
 import LoadingScreen from "./components/loading-screen"
+import { auth } from "./firebase"
 
 const router = createBrowserRouter([
   {
@@ -46,24 +47,30 @@ const GlobalStyles = createGlobalStyle`
   }
 `;
 
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+`;
+
 function App() {
   const [isLoading, setLoading] = useState(true);
   const init = async () => {
-    setTimeout(() => setLoading(false), 2000);
-    //setLoading(false);
+    await auth.authStateReady();
+    setLoading(false);
   };
   useEffect(() => {
     init();
   }, []);
   return (
-    <>
+    <Wrapper>
       <GlobalStyles />
       {
         isLoading ?
         <LoadingScreen /> :
         <RouterProvider router={router} />
       }
-    </>
+    </Wrapper>
   )
 }
 
--- src/firebase.ts
+++ src/firebase.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDG0yXsaSKPcOAy7chmFBS-Y_7V5QunZKw",
@@ -10,3 +11,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
