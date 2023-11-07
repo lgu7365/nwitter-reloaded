33f0f13ac0442a27bbb1aed6ccac6a220f6e9d58
unknown <lgu_lgu@naver.com>
2023-11-07 19:55:03 +0900

    create-account

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,86 +1,97 @@
 import { styled } from "styled-components";
 import { useState } from "react"
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth } from "../firebase";
+import { useNavigate } from "react-router-dom";
 
 const Wrapper = styled.div`
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 420px;
-    padding: 50px 0px;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 420px;
+  padding: 50px 0px;
 `;
 
 const Title = styled.h1`
-    font-size: 42px;
+  font-size: 42px;
 `;
 
 const Form = styled.form`
-    margin-top: 50px;
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
-    width: 100%;
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  width: 100%;
 `;
 
 const Input = styled.input`
-    padding: 10px 20px;
-    border-radius: 50px;
-    border: none;
-    width: 100%;
-    font-size: 16px;
-    &[type="submit"] {
-        cursor: pointer;
-        &:hover {
-        opacity: 0.8;
-        }
+  padding: 10px 20px;
+  border-radius: 50px;
+  border: none;
+  width: 100%;
+  font-size: 16px;
+  &[type="submit"] {
+    cursor: pointer;
+    &:hover {
+    opacity: 0.8;
     }
+  }
 `;
 
 const Error = styled.span`
-    font-weight: 600;
-    color: tomato;
+  font-weight: 600;
+  color: tomato;
 `;
 
 export default function CreateAccount() {
-    const [isLoading, setLoading] = useState(false);
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState("");
-    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const {
-            target: {name, value},
-        } = e;
-        if(name === "name") {
-            setName(value);
-        }
-        if(name === "email") {
-            setEmail(value);
-        }
-        if(name === "password") {
-            setPassword(value);
-        }
+  const navigate = useNavigate();
+  const [isLoading, setLoading] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: {name, value},
+    } = e;
+    if(name === "name") {
+      setName(value);
     }
-    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        try {
-
-        } catch(e){
-            
-        } finally {
-            setLoading(false);
-        }
+    if(name === "email") {
+      setEmail(value);
     }
-    return (
-        <Wrapper>
-            <Title>Log into X</Title>
-            <Form onSubmit={onSubmit}>
-                <Input name="name" value={name} placeholder="Name" type="text" onChange={onChange} required />
-                <Input name="email" value={email} placeholder="Email" type="email" onChange={onChange} required />
-                <Input name="password" value={password} placeholder="Password" type="password" onChange={onChange} required />
-                <Input type="submit" value={isLoading ? "Loading..." : "Create Account"} />
-            </Form>
-            {error !== "" ? <Error>{error}</Error> : null}
-        </Wrapper>
-    );
+    if(name === "password") {
+      setPassword(value);
+    }
+  }
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if(isLoading || name === "" || email === "" || password === "") return;
+    try {
+      setLoading(true);
+      const credentials = await createUserWithEmailAndPassword(auth, email, password);
+      console.log(credentials.user);
+      await updateProfile(credentials.user, {
+        displayName: name,
+      });
+      navigate("/");
+    } catch(e){
+        
+    } finally {
+      setLoading(false);
+    }
+  }
+  return (
+    <Wrapper>
+      <Title>Join X</Title>
+      <Form onSubmit={onSubmit}>
+        <Input name="name" value={name} placeholder="Name" type="text" onChange={onChange} required />
+        <Input name="email" value={email} placeholder="Email" type="email" onChange={onChange} required />
+        <Input name="password" value={password} placeholder="Password" type="password" onChange={onChange} required />
+        <Input type="submit" value={isLoading ? "Loading..." : "Create Account"} />
+      </Form>
+      {error !== "" ? <Error>{error}</Error> : null}
+    </Wrapper>
+  );
 }
