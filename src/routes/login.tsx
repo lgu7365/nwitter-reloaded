f128c59569dbe00429e1d25e41e1be1e79d82c36
unknown <lgu_lgu@naver.com>
2023-11-16 20:54:56 +0900

    login 화면

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,7 +1,56 @@
+import { useState } from "react"
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
+import { useNavigate, Link } from "react-router-dom";
+import { FirebaseError } from "firebase/app";
+import { Input, Switcher, Form, Title, Wrapper, Error } from "../components/auth-components";
+
 export default function Login() {
-    return (
-        <>
-            <h1>Login</h1>
-        </>
-    )
+  const navigate = useNavigate();
+  const [isLoading, setLoading] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: {name, value},
+    } = e;
+    if(name === "email") {
+      setEmail(value);
+    }
+    if(name === "password") {
+      setPassword(value);
+    }
+  }
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError("");
+    if(isLoading || email === "" || password === "") return;
+    try {
+      setLoading(true);
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/");
+    } catch(e){
+      if(e instanceof FirebaseError){
+        setError(e.message);
+      }
+    } finally {
+      setLoading(false);
+    }
+  }
+  return (
+    <Wrapper>
+      <Title>Log In</Title>
+      <Form onSubmit={onSubmit}>
+        <Input name="email" value={email} placeholder="Email" type="email" onChange={onChange} required />
+        <Input name="password" value={password} placeholder="Password" type="password" onChange={onChange} required />
+        <Input type="submit" value={isLoading ? "Loading..." : "Log In"} />
+      </Form>
+      {error !== "" ? <Error>{error}</Error> : null}
+      <Switcher>
+        Don't have an account?{" "}
+        <Link to="/create-account">Create one &rarr;</Link>
+      </Switcher>
+    </Wrapper>
+  );
 }
